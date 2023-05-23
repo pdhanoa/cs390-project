21cbc4d6ac754c885e655474597c5f772a0bf4d4
pdhanoa <dhanoa.ne@gmail.com>
2023-05-22 23:10:37 -0500

    Added dashboard UI, added interactive, double map to interface

--- MCM7/shiny_app/mcmf_app/app.R
+++ MCM7/shiny_app/mcmf_app/app.R
@@ -8,6 +8,7 @@
 #
 
 library(shiny)
+library(shinydashboard)
 library(ggplot2)
 library(tidyverse)
 library(dplyr)
@@ -20,6 +21,25 @@ data <- data %>% rename("program_price" = "Program Price",
                         "geographic_cname" = "Geographic Cluster Name",
                         "modality" = "Meeting Type",
                         "provides_transport" = "Program Provides Transportation")
+neigh_data <- read_delim(file = "../../data/neighborhoods.csv")
+neigh_data$Name <- toupper(neigh_data$Name)
+neigh_data <- neigh_data %>% rename("median_income" = " Median_Income ")
+neigh_data$median_income <- gsub("\\$", "", 
+                                     gsub(",", "", neigh_data$median_income))
+neigh_data$median_income <- trimws(neigh_data$median_income)
+
+neigh_data$median_income <- as.double(neigh_data$median_income)
+
+neigh_data$Internet <- as.double(gsub("%$", "", neigh_data$Internet))
+
+neigh_data$Vehicle <- as.double(gsub("%$", "", neigh_data$Vehicle))
+
+neigh_geodata <- counties %>%
+  # return all rows for candidate Biden and all columns from x and y
+  left_join(
+    neigh_data,
+    by = c("community" ="Name")
+  )
 
 data <- data %>% mutate(geographic_cname = recode(geographic_cname, 'Back of the Yards' = 'NEW CITY', 'Little Village' = 'SOUTH LAWNDALE'))
 data <- data %>% filter(`Min Age` < 25)
@@ -69,69 +89,94 @@ merge_data$key_neighborhood <- ifelse(merge_data$community %in% keyword_list, "f
 
 
 
+
+
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("MCMF Analysis Dashboard"),
-    tabsetPanel(
-      tabPanel("panel 1",
-        sidebarLayout(
-          sidebarPanel(
-            selectInput("feature_dropdown",
-                        "Please select a feature to view on the map:",
-                        choices = c("freq_free", "freq_online", "freq_transport")),
-            checkboxInput("key_focus",
-                          label = "Check to view the focus neighborhoods",
-                          value = FALSE)
-            # plotOutput("map")
-          ),
-          
-          
-          # Show a plot of the generated distribution
-          mainPanel(
-                plotOutput("map")
+ui <- dashboardPage(
+  skin = c("blue"),
+  dashboardHeader(title = "My CHI. My Future"),
+  dashboardSidebar(
+    sidebarMenu(
+      id = 'tabs',
+      menuItem("About", tabName = "about", icon = icon("list")),
+      menuItem("Widgets", icon = icon("th"), tabName = "widgets",
+               badgeLabel = "new", badgeColor = "green")
+    )),
+  dashboardBody(
+    tabItems(
+      tabItem("about",
+              box(plotOutput("map"), width = 6),
+              box(plotOutput("companion_map"), width = 6),
+              box(
+                selectInput("feature_dropdown",
+                            "Please select a feature to view on the map:",
+                            choices = c("freq_free", "freq_online", "freq_transport"),
+                            selected = "freq_free"),
+                checkboxInput("key_focus",
+                              label = "Check to view the focus neighborhoods",
+                              value = FALSE), width = 4
               )
-            )
-          ),
-      tabPanel("panel 2",
-            sidebarLayout(
-              sidebarPanel(),
-               mainPanel(
-               plotOutput("pie_chart")
-               )
-            )
-          ),
-      tabPanel("panel 3")
-    )
-)
+      ),
+      tabItem("widgets",
+      ),
+      tabItem("subitem1",
 
+      ),
+      tabItem("subitem2",
+              "Sub-item 2 tab content"
+      )
+    ),
+  ),
+  tags$head(
+    tags$style(HTML(".logo {background-color: #21b3ed !important;}
+                      .navbar {background-color: #21b3ed !important;}
+                     .skin_blue .main-sidebar .sidebar .sidebar-menu .active a {background-color: black;}
+                     .skin-blue .main-sidebar .sidebar .sidebar-menu a:hover {background-color : #d90023;}"))
+  )
+)
 
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  fill_var <- reactive({ input$feature_dropdown })
+  switch_var <- reactive({
+    switch(input$feature_dropdown, 
+           "freq_free" = "median_income",
+           "freq_online" = "Internet",
+           "freq_transport" = "Vehicle")}
+  )
+  output$map <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    p = merge_data %>%
+      ggplot(aes_string(fill=fill_var())) + 
+      geom_sf(linewidth = 0.5)
+    
+    
+    if(input$key_focus){
+      p = p + geom_sf(fill = "gray", linewidth = 0.5, data = merge_data %>% filter(merge_data$key_neighborhood == "non-focus"))
+    }
+    
+    p
+  })
   
   
-
-    fill_var <- reactive({ input$feature_dropdown })
-    output$map <- renderPlot({
-        # generate bins based on input$bins from ui.R
-      p = merge_data %>%
-        ggplot(aes_string(fill=fill_var())) + 
-          geom_sf(linewidth = 0.5)
-          
-      
-      if(input$key_focus){
-        p = p + geom_sf(fill = "gray", linewidth = 0.5, data = merge_data %>% filter(merge_data$key_neighborhood == "non-focus"))
-      }
-      
-      p
-    })
+  output$companion_map <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    neigh_geodata %>%
+      ggplot(aes_string(fill=switch_var())) + 
+      geom_sf(linewidth = 0.5)
     
-    output$pie_chart <- renderPlot({
-      ggplot(data = type_data)
-    })
     
+    # if(input$key_focus){
+    #   p = p + geom_sf(fill = "gray", linewidth = 0.5, data = neigh_geodata %>% filter(neigh_geodata$key_neighborhood == "non-focus"))
+    # }
+    
+    # p
+  })
+  
+  output$pie_chart <- renderPlot({
+    ggplot(data = type_data)
+  })
 }
 
 # Run the application 
